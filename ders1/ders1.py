209ed0f6d89da761eea81d7fbefc9a3ccb3484d9
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2025-05-08 01:02:50 +0300

    ders1 int,float,str,type(),operators explained.

---
+++ ders1/ders1.py
@@ -0,0 +1,43 @@
+# number = 12                   # int or integer
+# number_in_str = "12"          # here the variable type is str or string
+
+# -------------int()------------- 
+# pi = 3
+# print(pi)                # cikti veya output --> 3
+# print(type(pi))          # output --> <class 'int'>
+# -------------float()-------------
+# num = 4.0
+# print(num)              # cikti veya output --> 4.0
+# print(type(num))        #  # output --> <class 'float'>
+# -------------str()-------------
+
+# number = 13
+# new_number = '13'
+# print(type(number))              # output :  <class 'int'>
+# print(type(new_number))          # output :  <class 'str'>
+
+# -------------concatenate-------------
+
+# print("14"+3)                      # output : Hata verir !!! can only concatenate str (not "int") to str
+#                                    # bir str veri tipi ile bir int toplanamaz
+
+# print("nova" + " "+ "akademi")     # output : nova akademi
+
+
+# ---------Python Arithmetic Operators--------
+
+# +	Addition (Toplama)	x + y	
+
+# -	Subtraction (Cikarma)	x - y	
+
+# *	Multiplication (Carpma)	x * y	
+
+# /	Division (Bolme)	x / y	
+
+# %	Modulus (modul)	x % y	
+
+# **	Exponentiation (Ust)	x ** y	
+# --> ornek : 3 ** 2 --> 3 uzeri 2 --> 9
+
+# //	Floor division	(Kesme) x // y 
+# --> ornek : 3 // 2 --> 1 uzeri 10 // 3 --> 3
